826119d62c3596c7bb5fda39ac3828c7e39e43c1
Samantha Morco <samanthamorco@gmail.com>
2017-05-10 12:52:43 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/first_recipe", to: 'recipes#one_recipe_method'
+  get "/first_recipe", to: "recipes#one_recipe_method"
 end
