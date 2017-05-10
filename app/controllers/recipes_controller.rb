dd8ceab75973236b77cfeb822bdd0c671aacb448
Samantha Morco <samanthamorco@gmail.com>
2017-05-10 19:16:39 -0400

    add form to create new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,14 @@ class RecipesController < ApplicationController
     @recipe = Recipe.first
     render "first_recipe_page.html.erb"
   end
+
+  def new_recipe_method
+    render "new_recipe_form.html.erb"
+  end
+
+  def create_recipe_method
+    @recipe = Recipe.new(title: params[:title_param], chef: params[:chef_param], prep_time: params[:prep_time_param])
+    @recipe.save
+    render "recipe_result.html.erb"
+  end
 end
