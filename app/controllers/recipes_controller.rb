dc83b93087e98cb0156216d1b15eebae24a8a45d
Samantha Morco <samanthamorco@gmail.com>
2017-05-11 18:23:02 -0400

    index action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,6 @@
 class RecipesController < ApplicationController
-  def one_recipe_method
-    @recipe = Recipe.first
-    render "first_recipe_page.html.erb"
-  end
-
-  def new_recipe_method
-    render "new_recipe_form.html.erb"
-  end
-
-  def create_recipe_method
-    @recipe = Recipe.new(title: params[:title_param], chef: params[:chef_param], prep_time: params[:prep_time_param])
-    @recipe.save
-    render "recipe_result.html.erb"
+  def index
+    @recipes = Recipe.all
+    render "index.html.erb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/first_recipe", to: "recipes#one_recipe_method"
-
-  get "/new_recipe_form", to: 'recipes#new_recipe_method'
-  post "/recipe_form_result", to: 'recipes#create_recipe_method'
+  get "/recipes", to: 'recipes#index'
 end
