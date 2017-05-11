aff8cd702af1c46da684ae7a944b3517e63a8b8a
Samantha Morco <samanthamorco@gmail.com>
2017-05-11 19:56:48 -0400

    implement show, edit, update actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,36 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
     render "index.html.erb"
   end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @recipe = Recipe.new(title: params[:title_param], chef: params[:chef_param], prep_time: params[:prep_time])
+    @recipe.save
+    render "create.html.erb"
+  end
+
+  def edit
+    @recipe = Recipe.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @recipe = Recipe.find_by(id: params[:id])
+    # if params[:title_params] != ""
+    # end
+    @recipe.title = params[:title_params]
+    @recipe.chef = params[:chef_params]
+    @recipe.ingredients = params[:ingredients_params]
+    @recipe.save
+    # @recipe.update(title: params[:title_params], chef: params[:chef_params], ingredients: params[:ingredients_params])
+    render "update.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/recipes", to: 'recipes#index'
+  get "/recipes/new", to: 'recipes#new'
+  get "/recipes/:id", to: 'recipes#show'
+  post "/recipes", to: 'recipes#create'
+  get "/recipes/:id/edit", to: 'recipes#edit'
+  patch "/recipes/:id", to: 'recipes#update'
 end
