88969598cad5241617f9b6e7686ac5055e19635d
Samantha Morco <samanthamorco@gmail.com>
2017-05-15 20:52:51 -0400

    style and model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,22 @@
 class Recipe < ApplicationRecord
-  
+  def ingredients_list
+    list = []
+    ingredients.split(", ").each do |ingredient|
+      list << ingredient.gsub(".", "")
+    end
+    list
+  end
+
+  def friendly_created_at
+    created_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hour".pluralize(hours) if hours > 0
+    result += " #{minutes} minute".pluralize(minutes) if minutes > 0
+    result
+  end
 end
